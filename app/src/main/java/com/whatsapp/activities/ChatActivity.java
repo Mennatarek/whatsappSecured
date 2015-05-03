1607f69fbbccb3b7ce25e57b53e9064679fb2a30
menna_tarek <mennatallah.dawood@gmail.com>
2015-05-03 07:25:28 +0200

    Fix Location + add file Attachment without the IOImage socket

--- app/src/main/java/com/whatsapp/activities/ChatActivity.java
+++ app/src/main/java/com/whatsapp/activities/ChatActivity.java
@@ -4,10 +4,23 @@ package com.whatsapp.activities;
  * Created by menna on 01/05/15.
  */
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.DataSetObserver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
@@ -29,8 +42,12 @@ public class ChatActivity extends ActivityTracker {
     private ListView listView;
     private EditText chatText;
     private Button buttonSend;
+    String  attachmentFile;
+    Uri URI = null;
+    private static final int PICK_FROM_GALLERY = 101;
 
     private boolean side = false;
+    private int columnIndex;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -100,6 +117,7 @@ public class ChatActivity extends ActivityTracker {
 
                 dos.writeUTF(chatText.getText().toString());
 
+
                 dos.close();
                 socket.close();
 
@@ -141,4 +159,83 @@ public class ChatActivity extends ActivityTracker {
         return true;
     }
 
+
+    public void location(View view){
+        LocationManager locationManager =
+                (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                String lat = Double.toString(location.getLatitude());
+                String lon = Double.toString(location.getLongitude());
+                chatText.setText("My Location is:" + lat + "--" + lon);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            public void onProviderEnabled(String provider) {}
+            public void onProviderDisabled(String provider) {}
+        };
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK) {
+            /**
+             * Get Path
+             */
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            attachmentFile = cursor.getString(columnIndex);
+            Log.e("Attachment Path:", attachmentFile);
+            URI = Uri.parse("file://" + attachmentFile);
+            Bitmap x = Bitmap.createBitmap(BitmapFactory.decodeFile(attachmentFile));
+            Drawable d = new BitmapDrawable(getResources(), x);
+            d.setBounds(0,0,chatText.getWidth(),0);
+            chatText.setCompoundDrawablesWithIntrinsicBounds(null, null,
+                    d, null);
+            cursor.close();
+        }
+    }
+
+    public void sendFile(View view){
+        openGallery();
+        if (shouldReturnResult()) {
+            Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND, URI); //or ACTION_PICK
+            this.setResult(Activity.RESULT_OK, shareIntent); //set result
+            this.finish(); //exit activity
+        } else {
+            Intent shareIntent = new Intent();
+            shareIntent.setPackage("com.whatsapp.activities");
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, URI);
+            shareIntent.setType("image/*");
+            startActivity(Intent.createChooser(shareIntent,attachmentFile));
+        }
+    }
+
+    public void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra("return-data", true);
+        startActivityForResult(
+                Intent.createChooser(intent, "Complete action using"),
+                PICK_FROM_GALLERY);
+    }
+
+    private boolean shouldReturnResult() {
+        Intent intent = getIntent();
+        if (intent!=null && intent.getType()!=null) {
+            return intent.getType().indexOf("image/") != -1;
+        } else {
+            return false;
+        }
+    }
+    
 }
